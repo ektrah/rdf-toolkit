9d1613ebb6852a64c966d84c56535984082bac06
Gabe Fierro <gtfierro@mines.edu>
2024-08-06 22:06:38 -0600

    add (deprecated) to search results

--- packages/cli/src/commands/make-site.tsx
+++ packages/cli/src/commands/make-site.tsx
@@ -228,6 +228,7 @@ interface SearchEntry {
     readonly "id": string;
     readonly "name": string;
     readonly "description"?: string;
+    readonly "deprecated"?: boolean;
 }
 
 function buildSearchEntryForClass(class_: Class, context: RenderContext): SearchEntry {
@@ -237,6 +238,7 @@ function buildSearchEntryForClass(class_: Class, context: RenderContext): Search
         id: class_.id.value,
         name: prefixedName ? prefixedName.prefixLabel + ":" + prefixedName.localName : class_.id.value,
         description: class_.description,
+        deprecated: class_.deprecated,
     };
 }
 
